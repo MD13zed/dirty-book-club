81f85651d960fcdb67a86f725b558d1af7e172c2
Mitchell Dirkz <mitchelld1302@gmail.com>
2026-05-21 18:06:56 -0400

    switch pool to pg

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -3,8 +3,6 @@ const { Pool } = require("pg");
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-  max: 5,
-  idleTimeoutMillis: 30000,
 });
 
 module.exports = pool;
